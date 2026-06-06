d21778e5c9c5d6bb20f1de625c8e253a951554b8
Dean Andrei Tavas <deantavas02@gmail.com>
2026-06-06 22:15:12 +0800

    fix

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
-# Run migrations
-php artisan migrate --force
+# Default port to 80 if PORT is not set
+export PORT=${PORT:-80}
+
+# Replace the port placeholder in Nginx config
+sed -i "s/PORT_PLACEHOLDER/${PORT}/g" /etc/nginx/nginx.conf
+
+# Run migrations (don't exit if it fails, just log it)
+echo "Running migrations..."
+php artisan migrate --force || echo "Migrations failed, continuing..."
 
 # Cache configuration and routes
+echo "Caching configuration..."
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
 # Start Supervisor
+echo "Starting Supervisor on port ${PORT}..."
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
